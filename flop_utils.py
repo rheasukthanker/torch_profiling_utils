ffbecaf8dc6368d2d4b9dc42cd6b65a0e9bb81da
Rhea Sukthanker <sukthank@informatik.uni-freiburg.de>
2024-02-16 13:44:23 +0100

    add flop profiler and fix latency profiler

--- flop_utils.py
+++ flop_utils.py
@@ -1,13 +1,19 @@
 from calflops import calculate_flops
-from transformers import LlamaTokenizer
-from transformers import LlamaForCausalLM
+from torchvision import models
+
+model = models.alexnet()
+batch_size = 1
+input_shape = (batch_size, 3, 224, 224)
+flops, macs, params = calculate_flops(model=model, 
+                                      input_shape=input_shape,
+                                      output_as_string=True,
+                                      output_precision=4)
+print("Alexnet FLOPs:%s   MACs:%s   Params:%s \n" %(flops, macs, params))
+
+from calflops import calculate_flops_hf
 
 batch_size, max_seq_length = 1, 128
-model_name = "llama2_hf_7B"
-model_save = "../model/" + model_name
-model = LlamaForCausalLM.from_pretrained(model_save)
-tokenizer = LlamaTokenizer.from_pretrained(model_save)
-flops, macs, params = calculate_flops(model=model,
-                                      input_shape=(batch_size, max_seq_length),
-                                      transformer_tokenizer=tokenizer)
-print("Llama2(7B) FLOPs:%s   MACs:%s   Params:%s \n" %(flops, macs, params))
+model_name = "baichuan-inc/Baichuan-13B-Chat"
+
+flops, macs, params = calculate_flops_hf(model_name=model_name, input_shape=(batch_size, max_seq_length))
+print("%s FLOPs:%s  MACs:%s  Params:%s \n" %(model_name, flops, macs, params))
--- latency_profile_utils.py
+++ latency_profile_utils.py
@@ -4,23 +4,24 @@ from torch.profiler import profile, record_function, ProfilerActivity
 import numpy as np
 import torchvision.models as models
 model = models.resnet18().cuda()
-inputs = torch.randn([16,3,224,224]).cuda()
+inputs = torch.randn([2,3,224,224]).cuda()
 times_profiler_cpu = []
 times_profiler_gpu = []
 def get_latency_from_string(s, sub_str= "CPU time total: "):
   index = s.find(sub_str)  # find the index of the substring
   if index != -1:  # check if substring is found
-    if s[-1:] == "s":
+    print(s[-2])
+    if s[-2] == "s" and s[-3:-1] != "ms":
       unit = "s"
-      content = s[index + len(sub_str):-1]  # extract content following substring
+      content = s[index + len(sub_str):-3]  # extract content following substring
     else:
-       unit = s[-3:]
+       unit = s[-3:-1]
        content = s[index + len(sub_str):-3]  # extract content following substring
     #print(content)  # prints " jumps over the lazy dog"
     return content, unit
   else:
     print("Substring not found")
-for i in range(100):
+for i in range(10):
   with profile(activities=[ProfilerActivity.CPU,ProfilerActivity.CUDA], record_shapes=True) as prof:
     with record_function("model_inference"):
         model(inputs)
@@ -31,7 +32,7 @@ for i in range(100):
   times_profiler_gpu.append(float(time_gpu))
 model = model.cpu()
 inputs = inputs.cpu()
-for i in range(100):
+for i in range(10):
   with profile(activities=[ProfilerActivity.CPU], record_shapes=True) as prof:
     with record_function("model_inference"):
         model(inputs)
@@ -42,3 +43,6 @@ print("Mean time on cpu {} {}".format(np.mean(times_profiler_cpu),unit_cpu))
 print("Standard deviation time on cpu {} {}".format(np.std(times_profiler_cpu), unit_cpu))
 print("Mean time on gpu {} {}".format(np.mean(times_profiler_gpu),unit_gpu))
 print("Standard deviation time on gpu {} {}".format(np.std(times_profiler_gpu), unit_gpu))
+
+#Large Languase Model, such as llama2-7b.
+
